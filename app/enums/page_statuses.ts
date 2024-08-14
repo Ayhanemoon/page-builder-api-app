15f722124a06967a6394a7ee2d7deee5ec637e38
ayhan.samimi <ayhan.samimi@gmail.com>
2024-08-14 19:59:24 +0330

    feat: roles and page_statuses enums added

---
+++ app/enums/page_statuses.ts
@@ -0,0 +1,7 @@
+enum PageStatuses {
+    Draft=1,
+    Published=2,
+    Archive=3
+}
+
+export default PageStatuses
---
+++ app/enums/roles.ts
@@ -0,0 +1,7 @@
+enum Roles {
+    Admin = 1,
+    Creator=2,
+    User = 3,
+}
+
+export default Roles
